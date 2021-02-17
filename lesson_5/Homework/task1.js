1f0a941ba2f78a4f26818938848b7e6ced7c0b74
Oleksandr-Heleta <sanock.geleta@gmail.com>
2021-02-17 11:22:05 +0200

    add Like Btn

--- lesson_5/Homework/task1.js
+++ lesson_5/Homework/task1.js
@@ -26,53 +26,76 @@
 
 
 */
-
+//  Конструктор обьекта:
 function Comment (name, text, avatarUrl ) {
   this.name = name,
   this.text = text,
   this.avatarUrl = avatarUrl,
   this.like = 0
 }
-
+// Прототип для Обьектов
 const zeroComment = {
   avatarUrl : "https://i.pinimg.com/originals/5a/f1/dd/5af1ddcde07255e8a999abcc061dd201.png" ,
-  addLike : function (like) {
-    this.like =+ like;
+  addLike : function () {
+    this.like += 1;
+    // console.log('addlike for',  this )
+    return this.like;
   }
 }
 
 Comment.prototype = zeroComment;
 // Object.setPrototypeOf( Comment, zeroComment );
 
+
+// Создание Обьектов
 const myComment1 = new Comment('user1', 'all good', 'https://proslang.ru/wp-content/uploads/2019/03/avatarka_1-300x300.jpg');
-console.log(myComment1);
-myComment1.addLike(10);
-console.log(myComment1.like);
+// console.log(myComment1);
+
 const myComment2 = new Comment('user2', 'all bad', 'https://i.pinimg.com/originals/9c/77/46/9c7746225873e02d83b9315501b8dd2f.jpg');
 const myComment3 = new Comment('user3', 'good quality', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_RfqA1o6QkxYWBxk_-TCF28EBlasrxI-wWQ&usqp=CAU');
 const myComment4 = new Comment('user4', 'sow sow');
 
-myComment4.addLike(30);
+// myComment4.addLike();
+// myComment4.addLike();
+// myComment4.addLike();
 delete myComment4.avatarUrl;
-console.log(myComment4);
+// console.log(myComment4);
 
+// Масив обьектов
 const CommentsArray = [myComment1, myComment2, myComment3, myComment4];
 const wrapper = document.querySelector('.wrapper')
 
+// Функция создания коментариев
 const createComentsList = function (arr){
   arr.forEach(element => {
-    console.log(element.name)
+    // console.log(element.name)
     const comentBlock = document.createElement('div');
     comentBlock.innerHTML = `
     <div >
        <img class="img" src=${element.avatarUrl} />
     </div>
     <div class="text" >
-      <span> ${element.name} : </span><span> ${element.text}  </span><br/><span> Like: ${element.like}  </span>
+      <span> ${element.name} : </span><span> ${element.text}  </span>
+      <br/>
+      <button id="${arr.indexOf(element)}">Like:</button><span id="numb${arr.indexOf(element)}">  ${element.like}  </span>
     </div>`;
     comentBlock.className = 'comentBlock';
     wrapper.appendChild(comentBlock)
+
+    // Навешивание функции на кнопку Like
+    const like = document.getElementById(arr.indexOf(element));
+    const likeNumb = document.getElementById(`numb${arr.indexOf(element)}`);
+    
+    const likeAdd = element.addLike.bind(element);
+    const newLike = () => {
+      likeAdd()
+      likeNumb.innerText = element.like;
+      // console.log(element.like)
+    }
+    like.addEventListener('click', newLike);
   });
 }
 
+// Запуск функции создания коментариев
 createComentsList(CommentsArray);
+
