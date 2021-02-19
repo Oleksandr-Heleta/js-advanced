2240a616889971a8f7af9a09a2ce6151e50160f7
Oleksandr-Heleta <sanock.geleta@gmail.com>
2021-02-19 23:17:29 +0200

    task4 is done

--- lesson_6/Classwork/task4.js
+++ lesson_6/Classwork/task4.js
@@ -14,15 +14,61 @@
         * Изменять HTML можно.
 
     */
-   const assigned = document.querySelectorAll('.assigned');
+  //  const assigned = document.querySelectorAll('.assigned');
    const ObjForm = document.querySelectorAll('.ObjForm');
    const createdObj = document.querySelectorAll('.createdObj');
    const result = document.getElementById('result');
    const combineObj = document.getElementById('combineObj');
 
+const firstObj = {};
+const secondObj = {};
+let resultObj = {};
+
+
    ObjForm.forEach( e => {
-       e.key.addEventListener('change', function keyFunction() {
-            let key = e.key.value;
-            
+     e.btn.addEventListener('click', function keyFunction(evt) {
+      evt.preventDefault();
+      let key = e.key.value;
+      let value = e.value.value;
+      e.key.value = "";
+      e.value.value = "";
+      //  console.log(e);
+      let assigned = e.closest('.assigned');
+      //  console.log(assigned);
+      let id = assigned.getAttribute('id');
+      let ul = assigned.querySelector('ul');
+      //  console.log(id);
+       ul.innerHTML = "";
+       if (id === '1') {
+         firstObj[key] = value;
+         for (let key in firstObj) {
+           let li = document.createElement('li');
+           li.innerText = `${key}: ${firstObj[key]},`;
+           ul.appendChild(li);
+           console.log(li);
+         }
+       } else { 
+         secondObj[key] = value;
+         for (let key in secondObj) {
+           let li = document.createElement('li');
+           li.innerText = `${key}: ${secondObj[key]},`;
+           ul.appendChild(li);
+           console.log(li);
+         }
+       }
+       
        })
    })
+
+combineObj.addEventListener('click', function combineFunction() {
+  resultObj = Object.assign({}, firstObj, secondObj);
+  result.innerHTML = "";
+  let ul = document.createElement('ul');
+  for (let key in resultObj) {
+      let li = document.createElement('li');
+      li.innerText = `${key}: ${resultObj[key]},`;
+      ul.appendChild(li);
+      console.log(li);
+  }
+  result.appendChild(ul);
+})
