915e970707904bf22edba42bf8ac0d6a64aca8be
Oleksandr-Heleta <sanock.geleta@gmail.com>
2021-03-05 16:35:41 +0200

    to do ui and checking

--- courseWork/script.js
+++ courseWork/script.js
@@ -6,7 +6,10 @@ let posts = [];
 let id = 0;
 console.log(postForm.send);
 postForm.send.addEventListener('click', createPost);
-loadImgBtn.addEventListener('click', loadImg);
+loadImgBtn.addEventListener('click', e =>{
+    e.preventDefault();
+    loadImg();
+});
 
 class Posts  {
     constructor(id, author, date, text, imgUrl, likes, countComments, comments){
@@ -21,6 +24,8 @@ class Posts  {
       this.comments = comments ? comments : []; 
      
       this.addLike = this.addLike.bind(this);
+      this.showForm = this.showForm.bind(this);
+      this.sendAnswer = this.sendAnswer.bind(this);
       this.render();
     }
 
@@ -37,6 +42,13 @@ class Posts  {
 
         localStorage.setItem('posts', JSON.stringify(posts));
     };
+
+    showForm(e) {
+        e.preventDefault();
+        const showForm =  document.getElementById(`create_comment_for${this.id}`);
+        // console.log(showForm);
+        showForm.style.display = "block";
+    }
     render() {
         
         let postListItem = document.createElement('li');
@@ -55,28 +67,63 @@ class Posts  {
                         <button class="likes" id="addLike">Likes: ${this.likes}</button>
                         <div>
                             <span>Comments(${this.countComments})</span>
-                            <button>Write comment</button>
+                            <button id="openForm">Write comment</button>
                         </div>
                     </div>
                     <ul class="comments">
                         
                      </ul>
-                    <form class="comment_form" id="create_post">
+                    <form class="comment_form" id="create_comment_for${this.id}">
                         <input type="text" placeholder="Author"  id="author_inp" name="author">
                         <br>
                         <textarea placeholder="Comment" id="about_inp" name="about"></textarea>
                         <br>
-                        <button class="btn_send" name="send">Send Post</button>
+                        <button class="btn_send" name="send">Send Comment</button>
                     
                     </form>
             `;
-        postListItem.dataset.id = this.id;
-        
+        // postListItem.dataset.id = this.id;
+        postListItem.setAttribute('id', this.id)
+        postListItem.classList.add("post_item");
 
-        ulPosts.appendChild(postListItem);
+        ulPosts.prepend(postListItem);
         const likeBtn = postListItem.querySelector('#addLike');
+        const comentFormBtn = postListItem.querySelector('#openForm');
+        const commentCreateBtn = postListItem.querySelector('.btn_send');
         likeBtn.addEventListener('click', this.addLike );
+        comentFormBtn.addEventListener('click', this.showForm);
+        commentCreateBtn.addEventListener('click', this.sendAnswer);
     };
+
+    sendAnswer(e) {
+        e.preventDefault();
+        const postListItem = document.getElementById(this.id);
+        const commentForm = postListItem.querySelector('.comment_form');
+         id++
+         this.countComments++;
+        let author = commentForm.author.value;
+        let text = commentForm.about.value;
+        
+        let date = getDay()
+        let parentId = this.id;
+
+        let comment = new Comment(id, author, text, date, parentId);
+        this.comments.push(comment);
+        
+        comment.render();
+        commentForm.style.display = "none";
+        commentForm.about.value = "";
+        commentForm.author.value = "";
+
+        posts.forEach( post => {
+            if (post.id === this.id) {
+                post.comments = this.comments;
+                post.countComments = this.countComments;
+            }
+        });
+
+        localStorage.setItem('posts', JSON.stringify(posts));
+      }
 };
 
 function createPost(e) {
@@ -84,7 +131,7 @@ function createPost(e) {
     let author = postForm.author.value;
     let text = postForm.text.value;
     let date = getDay();
-    let imgUrl = postForm.imageURl.value;
+    let imgUrl = loadImg();
     id++;
     let post = new Posts(id, author, date, text, imgUrl);
     
@@ -93,7 +140,7 @@ function createPost(e) {
     localStorage.setItem('posts', JSON.stringify(posts));
     postForm.author.value = "";
     postForm.text.value = "";
-    postForm.imageURl.value = loadImg();
+    postForm.imageURl.value ="";
 }
 
 function getDay()  {
@@ -112,15 +159,47 @@ function getDay()  {
     return date;
 }
   
-function loadImg(e) {
-    e.preventDefault();
+function loadImg() {
     let urlInput = document.getElementById('img_inp');
-    let preview = document.querySelector('.preview');
+    let preview = document.querySelector('.preview_img');
     if (urlInput.value !== "") {  
         // console.log(preview);
         preview.src = urlInput.value;
     } else {
-        preview.src = 'https://фонетический-разбор.рф/wp-content/themes/slova/img/no-photo.jpg';
+        preview.src = 'img/default-image.jpg';
     }
     return preview.src
 }
+
+class Comment {
+    constructor(id, author, text, date, parentId) {
+        this.id = id;
+        this.author = author; 
+        this.text = text;  
+        this.date = date; 
+        this.parentId = parentId;
+    }
+  
+     render () {
+          const postListItem = document.getElementById(this.parentId);
+          const commentList = postListItem.querySelector('.comments');
+          let commentListItem = document.createElement('li');
+          commentListItem.innerHTML = `
+            <div class="author_date">
+                <div class="author">
+                 ${this.author}
+                </div>
+                <div class="date">
+                ${this.date}
+                </div>
+            </div>
+     
+            <div class="post_text">
+                ${this.text}
+            </div>
+              `;
+            commentListItem.setAttribute('id', this.id);
+
+          commentList.prepend(commentListItem);
+        }  
+  }
